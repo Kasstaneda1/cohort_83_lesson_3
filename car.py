d3326028f8c7b9d6a5723ae07e28fc0650b530f1
Kasstaneda1 <mihailcub@gmail.com>
2026-06-23 21:06:53 -0400

    add s and c

---
+++ car.py
@@ -0,0 +1,9 @@
+car = [
+    {
+        "First name": "John",
+        "Last name": "Johnson",
+        "age": 62,
+        "group": "group 83",
+        "course": "Frontend"
+    }
+]
--- students.py
+++ students.py
@@ -5,5 +5,12 @@ students = [
         "age": 32,
         "group": "group 83",
         "course": "Frontend"
+    },
+    {
+        "First name": "Smith",
+        "Last name": "Somovich",
+        "age": 44,
+        "group": "group 83",
+        "course": "PR manager"
     }
 ]
